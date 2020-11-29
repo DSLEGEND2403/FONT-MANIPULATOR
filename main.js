95666478d8583e4814053d7bb89126ea237075d8
DSLEGEND2403 <71708093+DSLEGEND2403@users.noreply.github.com>
2020-11-29 15:47:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() 
+{
+video=createCapture(VIDEO);
+video.size(500, 500);
+canvas=createCanvas(550, 550);
+canvas.position(560,150); 
+poseNet= ml5.poseNet(video, modelLoaded); 
+poseNet.on('pose', gotPoses); 
+}
+
+function draw()
+{
+background('#969A97');
+document.getElementById("span").innerHTML = "Size of font will be =" +difference+"px";
+fill('#F90093');
+stroke('F90093');
+square(wristX, wristY, difference);    
+}
+
+function modelLoaded()
+{
+console.log('Posenet Is Initialized')    
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+console.log(results); 
+noseX = results[0].pose.wrist.x;
+noseY = results[0].pose.wrist.y;
+console.log("wristX = " +wrisyX+ "wristY="+ wristY);
+}
+}
